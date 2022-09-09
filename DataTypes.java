2dfc3d122c762cab556fc0f9a87365b7b9af389d
Manovadhana M <113025165+Manojega0311@users.noreply.github.com>
2022-09-08 22:33:11 -0700

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+//wro to demo dataypes
+//Manovadhana M CG batch 2576
+class DataTypes
+{
+
+public static void main(String[]args)
+
+{
+
+double b = 123.43555;
+char c = 'e';
+boolean d = true;
+int a = 10;
+float f = 9.87f;
+byte g = 126;
+String s = (" My first program ");
+
+System.out.println("Duouble: " + b);
+System.out.println("Character: " + c);
+System.out.println("Boolean: " + d);
+System.out.println("Integer: " + s);
+System.out.println("Float: " + f);
+System.out.println("Byte: " + b);
+System.out.println("String: " + s);
+
+}
+}
